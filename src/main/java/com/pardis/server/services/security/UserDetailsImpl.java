bd5087d9e3f0794962950bd7f753dad6c268abd2
Azamnia <mohammad.ali.azamnia@gmail.com>
2018-12-24 22:05:20 +0330

    Update UserDetailsImpl.java

--- src/main/java/com/pardis/server/services/security/UserDetailsImpl.java
+++ src/main/java/com/pardis/server/services/security/UserDetailsImpl.java
@@ -1,4 +1,4 @@
-package guru.springframework.services.security;
+package com.pardis.server.services.security;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
