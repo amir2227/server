641b8393737ea28107b3e5d7adbe6ef1fcc0d927
Azamnia <mohammad.ali.azamnia@gmail.com>
2018-12-24 21:06:03 +0330

    Create RoleRepository.java

---
+++ src/main/java/com/pardis/server/repositories/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.pardis.server.repositories;
+
+import com.pardis.server.model.Role;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoleRepository extends CrudRepository<Role, Integer>{
+}
