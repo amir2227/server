bd10fc832e05f40c0d9ef06d1e5f0cae8a527098
Azamnia <mohammad.ali.azamnia@gmail.com>
2018-12-24 20:49:47 +0330

    Update SpringSecConfig.java

--- src/main/java/com/pardis/server/config/SpringSecConfig.java
+++ src/main/java/com/pardis/server/config/SpringSecConfig.java
@@ -1,4 +1,4 @@
-package guru.springframework.config;
+package com.pardis.server.config;
 
 import org.jasypt.springsecurity3.authentication.encoding.PasswordEncoder;
 import org.jasypt.util.password.StrongPasswordEncryptor;
