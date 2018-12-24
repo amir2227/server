d7e3d273e25ae1bb4e50a77c661a6152d7d46d8c
Azamnia <mohammad.ali.azamnia@gmail.com>
2018-12-24 21:53:45 +0330

    Update RoleServiceMapImpl.java

--- src/main/java/com/pardis/server/services/mapservices/RoleServiceMapImpl.java
+++ src/main/java/com/pardis/server/services/mapservices/RoleServiceMapImpl.java
@@ -1,11 +1,9 @@
-package guru.springframework.services.mapservices;
+package com.pardis.server.services.mapservices;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-/**
- * Created by jt on 12/18/15.
- */
+
 @Service
 @Profile("map")
 public class RoleServiceMapImpl extends AbstractMapService {
