52fa53619b270f3665af0a12779477123b44a051
Azamnia <mohammad.ali.azamnia@gmail.com>
2019-01-05 12:06:55 +0330

    Update User.java

--- src/main/java/com/pardis/server/model/User.java
+++ src/main/java/com/pardis/server/model/User.java
@@ -8,11 +8,14 @@ import java.util.List;
 @Entity
 public class User extends AbstractDomainClass  {
 
+    private Long userId;
     private String username;
-
-    @Transient
     private String password;
-
+    private String location;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String token;
     private String encryptedPassword;
     private Boolean enabled = true;
 
@@ -22,13 +25,82 @@ public class User extends AbstractDomainClass  {
     //     inverseJoinColumns = @joinColumn(name = "role_id"))
     private List<Role> roles = new ArrayList<>();
     private Integer failedLoginAttempts = 0;
+    
+    
+    public User() {
+    	 
+    }
+ 
+    public User(Long userId, String username, String firstName, String lastName, 
+            boolean enabled, String email, String location, String encrytedPassword) {
+        super();
 
-    public String getUsername() {
+        this.userId = userId;
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.enabled = enabled;
+        this.email = email;
+        this.location = location;
+        this.encryptedPassword = encrytedPassword;
+
+    }
+    
+    public Long getUserId() {
+        return userId;
+    }
+ 
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+    
+
+    public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getUsername() {
         return username;
     }
 
     public void setUsername(String username) {
         this.username = username;
+    }
+    
+    public String getFirstName() {
+        return firstName;
+    }
+ 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+ 
+    public String getLastName() {
+        return lastName;
+    }
+ 
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+ 
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getPassword() {
